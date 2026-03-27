2af3774856807a5f88476ae0c0faaaa3918bde48
Satyam Yadav <satyamyadavmau01@gmail.com>
2026-03-28 00:43:16 +0530

    Add files via upload

---
+++ coding_exercise13.py
@@ -0,0 +1,12 @@
+row1=['😊' '😊' '😊']
+row2=['😊' '😊' '😊']
+row3=['😊' '😊' '😊']
+matrix=[row1,row2,row3]
+print(f"{row1}\n{row2}\n{row3}")
+position=input("Enter the position where you want to hide money:")
+#32
+row_number=int(position[0])
+column_number=int(position[1])
+row_selected=matrix[row_number-1]
+row_selected[column_number-1]='X'
+print(f"{row1}\n{row2}\n{row3}")
