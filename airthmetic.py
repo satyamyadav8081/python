9d1f1b482ff14c826cc17f656c0db9f847df9ce5
Satyam Yadav <satyamyadavmau01@gmail.com>
2026-04-02 00:18:20 +0530

    Add files via upload

---
+++ airthmetic.py
@@ -0,0 +1,34 @@
+a,b,c=5,4,6
+print(a,b,c)
+
+
+a,b=4,3
+c=a+b
+a+= 2
+c //= a
+print(c)
+
+
+
+a=5
+print(a==5)
+print(a<=5)
+print(a<5)
+print(a !=6  )
+      
+a,b=4,3
+print(a<3 and b==3)
+
+a,b=4,3
+print(a<3 or b==3)
+
+
+a,b=4,3
+c=True
+print(not(c))
+
+a,b=5,4
+print(a & b)
+print(a|b)
+print(a ^ b)
+print(~a)
