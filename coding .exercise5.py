c59878825976a7db0327cceb3d4a6fb3515bcb92
Satyam Yadav <satyamyadavmau01@gmail.com>
2026-04-02 00:17:37 +0530

    Add files via upload

---
+++ coding .exercise5.py
@@ -0,0 +1,8 @@
+age=int(input("Enter your age:"))
+
+years_left=90-age 
+days_left = years_left * 365
+months_left = years_left * 12
+weeks_left = years_left * 52
+
+print(f"You have {days_left} days, {weeks_left} week and {months_left} months left.") 
