cfe03e6b852d721a63a9ba6e3b2afdb4a68f186d
Satyam Yadav <satyamyadavmau01@gmail.com>
2026-05-30 10:31:21 +0530

    Add files via upload

---
+++ while_loop.py
@@ -0,0 +1,23 @@
+#number=int(input("enter number(-1 to quit)"))
+#count=1
+#while count<=5:
+#while number !=-1:
+    #print(number)
+    #number = int(input("enter number(-1 to quit)"))
+
+    #print(count)
+    #count +=1
+    #if count==3:
+        #break
+#else:
+    #print("in else block")    
+#print("out from loop")
+count=0
+while True:
+    print(count)
+    count+=1   
+    if count==5:
+        break
+else:
+    print("in else block")
+print("out from loop")         
